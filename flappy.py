48e59572df00e818beb18f2ac72dc2cde8df53ab
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-09 11:29:02 -0300

    Implementado o chão

--- flappy.py
+++ flappy.py
@@ -5,6 +5,10 @@ SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 800
 SPEED = 10
 GRAVITY = 1
+GAME_SPEED = 10
+
+GROUND_WIDTH = 2 * SCREEN_WIDTH
+GROUND_HEIGHT = 100
 
 class Bird(pygame.sprite.Sprite):
     
@@ -39,6 +43,24 @@ class Bird(pygame.sprite.Sprite):
     def bump(self):
         self.speed = -SPEED
 
+class Ground(pygame.sprite.Sprite):
+
+    def __init__(self, xpos):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('sprites/base.png')
+        self.image = pygame.transform.scale(self.image, (GROUND_WIDTH, GROUND_HEIGHT))
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+        self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
+def is_off_sreen(sprite):
+    return sprite.rect[0] < -(sprite.rect[2])
+
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -52,6 +74,11 @@ bird_group.add(bird)
 
 clock = pygame.time.Clock()
 
+ground_group = pygame.sprite.Group()
+for i in range(2):
+    ground = Ground(GROUND_WIDTH * i)
+    ground_group.add(ground)
+
 while True:
     clock.tick(30)
     for event in pygame.event.get():
@@ -64,8 +91,16 @@ while True:
 
     screen.blit(BACKGROUND, (0, 0))
 
+    if is_off_sreen(ground_group.sprites()[0]):
+        ground_group.remove(ground_group.sprites()[0])
+
+        new_ground = Ground(GROUND_WIDTH - 20)
+        ground_group.add(new_ground)
+
     bird_group.update()
+    ground_group.update()
 
     bird_group.draw(screen)
+    ground_group.draw(screen)
 
     pygame.display.update()
